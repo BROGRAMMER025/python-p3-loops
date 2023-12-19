4a37cf138f9765c0463150b5b769f047b9ab7e6e
allan murigi <allan.murigi@student.moringaschool.com>
2023-12-19 18:48:32 +0300

    looping in python

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,42 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    count = 10
+
+    while count >= 1:
+        print(count)
+        count -= 1
+
+    print("Happy New Year!")
+happy_new_year()    
+    
+
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_list = [x ** 2 for x in int_list]
+    return squared_list
+
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [-1, -2, -3, -4, -5]
+
+result1 = square_integers(list1)
+result2 = square_integers(list2)
+
+print("Squared elements of list1:", result1)
+print("Squared elements of list2:", result2)
 
 def fizzbuzz():
     # code goes here!
-    pass
+     for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+fizzbuzz()
